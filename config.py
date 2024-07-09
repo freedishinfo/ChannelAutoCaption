5e4b245a653deb2a034843da34eac4d0c6d7828b
freedishinfo <166800049+freedishinfo@users.noreply.github.com>
2024-07-09 18:15:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    DB_URL = os.environ.get("DATABASE_URL", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7218167291:AAGPEvK5-G3JYaRyJdWg1dHur34J5yZUDOY")
+    API_ID = int(os.environ.get("API_ID", "1310650"))
+    API_HASH = os.environ.get("API_HASH", "8b85f95e0e07d0aee4fa812ce9ea46f4")
+    DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://freedishinfo2022:BJYtDeJ8c8QBi9tW@xyzdata.bwhb1tl.mongodb.net/?retryWrites=true&w=majority&appName=xyzdata")
